17f88175b58d4f76c11f434722191e785f883f8f
matteo.bertamini <matteo.bertamini@telefonica.com>
2022-02-24 00:02:20 +0100

    next framework setup

--- components/ArticleItem.tsx
+++ components/ArticleItem.tsx
@@ -3,13 +3,15 @@ import {FC} from "react";
 import Link from 'next/link';
 
 type Props = {
-    article: Object
+    article: any
 };
 
-const ArticleItem:FC<Props> = ({article}) => {
-    return(<Link href='/article/[id]' as={`/article/${article.id}`}>
-        <a className={articleStyles.card}><h3>{article.title} &rarr;</h3>
-        <p>{article.body}</p>
+const ArticleItem:FC<Props> = ({article: any}) => {
+    // @ts-ignore
+    const {body, id, title} = article;
+    return(<Link href='/article/[id]' as={`/article/${id}`}>
+        <a className={articleStyles.card}><h3>{title} &rarr;</h3>
+        <p>{body}</p>
         </a>
     </Link>);
 };
