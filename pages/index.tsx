5e2ac11198743efb8bc3268bc3f6fd60feb73f8d
matteo.bertamini <matteo.bertamini@telefonica.com>
2022-03-30 01:39:55 +0200

    improved layout without flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,20 @@
 import type { NextPage } from 'next';
 import classes from '../styles/Home.module.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
-  const contentRef = useRef<HTMLElement>(null);
   const [scroll, setScroll] = useState(0);
 
   useEffect(() => {
     function updatePosition() {
-      setScroll(contentRef.current?.scrollTop ?? 0);
+      setScroll(window.scrollY ?? 0);
     }
 
-    if (contentRef.current === null) {
-      return;
-    }
-
-    contentRef.current.addEventListener('scroll', updatePosition);
+    window.addEventListener('scroll', updatePosition);
     updatePosition();
 
-    return () =>
-      contentRef.current?.removeEventListener('scroll', updatePosition);
-  }, [contentRef.current]);
+    return () => window.removeEventListener('scroll', updatePosition);
+  }, []);
 
   return (
     <>
@@ -41,62 +35,60 @@ const Home: NextPage = () => {
           ].join(' ')}
         />
       </aside>
-      <div className={classes.content}>
-        <main ref={contentRef} id="tst" className={classes.dx}>
-          <article className={classes.cardSection}>
-            <h1 className={classes.nameTitle}>Matteo Bertamini</h1>
-            <p className={classes.jobDescription}>Fullstack Developer</p>
-          </article>
-          <article className={classes.presentationSection}>
-            <h2>Toolbox</h2>
-            <p className={classes.presentationDescription}>
-              I like the idea of a developer as an artisan who loves and cares
-              its product the most. <br />
-              Pushed by that mindset, I constantly grow and cover different
-              expertises.
-            </p>
-            <p className={classes.presentationDescription}>
-              With that in mind, this is what I currently have in my toolbox 🧑🏽‍💻:
-            </p>
-            <ul>
-              <li>
-                Frontend
-                <ul>
-                  <li>ES6: vanilla, Typescript, Flow.js</li>
-                  <li>React</li>
-                  <li>Next.JS</li>
-                </ul>
-              </li>
-              <li>
-                Backend
-                <ul>
-                  <li>Node</li>
-                  <li>Java: Kotlin, Groovy</li>
-                  <li>PHP: symfony</li>
-                  <li>C++: Qt</li>
-                </ul>
-              </li>
-              <li>
-                Cross
-                <ul>
-                  <li>Agile: kanban and scrum</li>
-                  <li>Kubernetes</li>
-                  <li>Hexagonal architecture</li>
-                  <li>DDD</li>
-                </ul>
-              </li>
-            </ul>
-          </article>
-          {/*<article style="height: 70vh; background-color: #DDF4C8; flex-shrink: 0; padding: 20px;">*/}
-          {/*    <p style="font-family: 'Alegreya', serif; font-size: 1.2em;">*/}
-          {/*        Come near, ladies and gentlemen!*/}
-          {/*    </p>*/}
-          {/*</article>*/}
-          <footer className={classes.footer}>
-            <p className={classes.footerContent}>Matteo Bertamini 2022</p>
-          </footer>
-        </main>
-      </div>
+      <main className={classes.content}>
+        <article className={classes.cardSection}>
+          <h1 className={classes.nameTitle}>Matteo Bertamini</h1>
+          <p className={classes.jobDescription}>Fullstack Developer</p>
+        </article>
+        <article className={classes.presentationSection}>
+          <h2>Toolbox</h2>
+          <p className={classes.presentationDescription}>
+            I like the idea of a developer as an artisan who loves and cares its
+            product the most. <br />
+            Pushed by that mindset, I constantly grow and cover different
+            expertises.
+          </p>
+          <p className={classes.presentationDescription}>
+            With that in mind, this is what I currently have in my toolbox 🧑🏽‍💻:
+          </p>
+          <ul>
+            <li>
+              Frontend
+              <ul>
+                <li>ES6: vanilla, Typescript, Flow.js</li>
+                <li>React</li>
+                <li>Next.JS</li>
+              </ul>
+            </li>
+            <li>
+              Backend
+              <ul>
+                <li>Node</li>
+                <li>Java: Kotlin, Groovy</li>
+                <li>PHP: symfony</li>
+                <li>C++: Qt</li>
+              </ul>
+            </li>
+            <li>
+              Cross
+              <ul>
+                <li>Agile: kanban and scrum</li>
+                <li>Kubernetes</li>
+                <li>Hexagonal architecture</li>
+                <li>DDD</li>
+              </ul>
+            </li>
+          </ul>
+        </article>
+        <div className={classes.footer}>
+          <p className={classes.footerContent}>Matteo Bertamini 2022</p>
+        </div>
+        {/*<article style="height: 70vh; background-color: #DDF4C8; flex-shrink: 0; padding: 20px;">*/}
+        {/*    <p style="font-family: 'Alegreya', serif; font-size: 1.2em;">*/}
+        {/*        Come near, ladies and gentlemen!*/}
+        {/*    </p>*/}
+        {/*</article>*/}
+      </main>
     </>
   );
 };
