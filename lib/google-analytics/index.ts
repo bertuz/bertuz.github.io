dbc116fa03e1ad83ef42e36b9d8a58105aa8a882
matteo.bertamini <matteo.bertamini@telefonica.com>
2022-04-03 04:20:07 +0200

    added Google Analytics

---
+++ lib/google-analytics/index.ts
@@ -0,0 +1,7 @@
+export const pageview = (url: string) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  window.gtag('config', process.env.NEXT_PUBLIC_MEASUREMENT_ID, {
+    path_url: url,
+  });
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,50 @@
 import '../styles/globals.css';
+import * as ga from '../lib/google-analytics';
+
 import Head from 'next/head';
+
+import Script from 'next/script';
+
+import { useRouter } from 'next/router';
+
+import { useEffect } from 'react';
 
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Matteo Bertamini</title>
       </Head>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
