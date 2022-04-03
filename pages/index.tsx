a27031faa8c1811898b77ff89a2a06cb2ed3acfb
matteo.bertamini <matteo.bertamini@telefonica.com>
2022-04-03 17:56:32 +0200

    started work experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import breakpoints from '../assets/styles/breakpoints';
 import { useEffect, useState } from 'react';
 import { css } from '@emotion/react';
 
+import Link from 'next/link';
+
 import type { NextPage } from 'next';
 
 // todo adopt csslint when available https://github.com/emotion-js/emotion/issues/2695
@@ -95,6 +97,48 @@ const useClasses = (showMac: boolean) => ({
       paddingLeft: 0,
     },
   }),
+  cvSection: css({
+    backgroundColor: '#C9B17D',
+    padding: 24,
+    fontFamily: "'Alegreya', serif",
+    fontSize: '1.2em',
+  }),
+  button: css({
+    appearance: 'button',
+    backgroundColor: 'transparent',
+    backgroundImage: 'linear-gradient(to bottom, #BDA675, #96845D)',
+    border: '0 solid #e5e7eb',
+    borderRadius: '.5rem',
+    boxSizing: 'border-box',
+    color: '#482307',
+
+    columnGap: '1rem',
+    cursor: 'pointer',
+    display: 'inline-block',
+    // font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    fontSize: '100%',
+    // font-weight: 700;
+    // line-height: 24px;
+    margin: 0,
+    outline: '2px solid transparent',
+    padding: '0.7rem 1.2rem',
+    textAlign: 'center',
+    textTransform: 'none',
+    transition: 'all .1s cubic-bezier(.4, 0, .2, 1)',
+    userSelect: 'none',
+    touchAction: 'manipulation',
+    boxShadow:
+      '-6px 8px 10px rgba(81,41,10,0.1),0px 2px 2px rgba(81,41,10,0.2)',
+    '&:focus': {
+      boxShadow:
+        'rgba(72, 35, 7, .46) 0 0 0 4px, -6px 8px 10px rgba(81,41,10,0.1), 0px 2px 2px rgba(81,41,10,0.2)',
+    },
+    '&:active': {
+      boxShadow:
+        'rgba(72, 35, 7, .46) 0 0 0 4px, -6px 8px 10px rgba(81,41,10,0.1), 0px 2px 2px rgba(81,41,10,0.2)',
+      backgroundImage: 'linear-gradient(to bottom, #AD996C, #96845D)',
+    },
+  }),
 });
 
 const Home: NextPage = () => {
@@ -182,6 +226,14 @@ const Home: NextPage = () => {
               </ul>
             </li>
           </ul>
+        </article>
+        <article css={classes.cvSection}>
+          <h2>Work Experience</h2>
+          <p>
+            <Link href=".">
+              <a css={classes.button}>Download CV</a>
+            </Link>
+          </p>
         </article>
         <div css={classes.footer}>
           <p css={classes.footerContent}>Matteo Bertamini 2022</p>
