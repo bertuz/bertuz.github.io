7265e1d7764bb024645245e9db7e10d80a9200bf
matteo.bertamini <matteo.bertamini@telefonica.com>
2022-04-08 18:48:00 +0200

    improved button for mobile

--- assets/styles/colors.ts
+++ assets/styles/colors.ts
@@ -3,6 +3,11 @@ const colors = {
   senape: '#EBB953',
   mountainGrey: '#CFC3B6',
   sugarPaperBlue: '#A3BEC4',
+  grey: '#6e6e6e',
+  darkerGrey: '#595959',
+  almostBlack: '#333',
+  almostWhite: '#e7e7e7',
+  totalBlack: 'black',
 };
 
 export default colors;
--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,9 @@
 import { dimensionInRem } from '../assets/styles/dimensions';
 
+import breakpoints from '../assets/styles/breakpoints';
+
+import colors from '../assets/styles/colors';
+
 import { css } from '@emotion/react';
 
 type ButtonProperties = {
@@ -13,11 +17,11 @@ const Button = ({ caption, iconPath }: ButtonProperties) => {
       css={css({
         display: 'inline-block',
         boxSizing: 'border-box',
-        border: '0px solid #333',
-        borderBottom: '3px solid #333',
+        border: `0px solid ${colors.almostBlack}`,
+        borderBottom: `3px solid ${colors.almostBlack}`,
         borderTop: '3px solid transparent',
-        backgroundColor: '#6e6e6e',
-        color: '#e7e7e7',
+        backgroundColor: colors.grey,
+        color: colors.almostWhite,
         fontFamily: 'Alegreya-Sans',
         fontSize: dimensionInRem(-1),
         borderRadius: dimensionInRem(-8),
@@ -26,28 +30,39 @@ const Button = ({ caption, iconPath }: ButtonProperties) => {
           cursor: 'pointer',
         },
         '&:active': {
-          border: '0px solid black',
-          borderTop: '3px solid black',
+          border: `0px solid ${colors.totalBlack}`,
+          borderTop: `3px solid ${colors.totalBlack}`,
           borderBottom: '3px solid transparent',
           paddingTop: '0.3rem',
           paddingBottom: '0.1rem',
-          backgroundColor: '#595959',
+          backgroundColor: colors.darkerGrey,
+        },
+        [breakpoints.maxMobile]: {
+          fontSize: dimensionInRem(0),
+          padding: `0.3rem 0.6rem`,
+          '&:active': {
+            paddingTop: '0.4rem',
+            paddingBottom: '0.2rem',
+          },
         },
       })}
     >
       {iconPath && (
         <img
-          alt=""
+          alt="download"
+          role="presentation"
           src={iconPath}
           style={{
             height: dimensionInRem(-1),
             paddingRight: '0.3rem',
             display: 'inline-block',
             verticalAlign: 'middle',
+            [breakpoints.maxMobile]: {
+              height: dimensionInRem(0),
+            },
           }}
         />
       )}
-
       {caption}
     </span>
   );
