18ffd970f8ea5005f62329befe585bb9567f7159
mbertamini <matteo@bertamini.net>
2022-05-12 16:40:43 +0200

    debug prod

--- pages/api/frontChat.ts
+++ pages/api/frontChat.ts
@@ -5,7 +5,7 @@ import {
 } from '../../components/chat/model';
 import clientPromise from '../../utils/api/db';
 
-import Pusher from 'pusher';
+// import Pusher from 'pusher';
 
 import type { Override } from '../../utils/types';
 
@@ -17,22 +17,22 @@ import type {
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const {
-  NEXT_PUBLIC_PUSHER_APP_KEY: channelKey,
-  PUSHER_APP_ID: channelAppId,
-  PUSHER_SECRET: channelAppSecret,
-  NEXT_PUBLIC_PUSHER_CLUSTER_REGION: channelCluster,
-  PUSHER_ENCRYPTION_MASTER_KEY: encryptionKey,
+  // NEXT_PUBLIC_PUSHER_APP_KEY: channelKey,
+  // PUSHER_APP_ID: channelAppId,
+  // PUSHER_SECRET: channelAppSecret,
+  // NEXT_PUBLIC_PUSHER_CLUSTER_REGION: channelCluster,
+  // PUSHER_ENCRYPTION_MASTER_KEY: encryptionKey,
   DB_DB_NAME: dbName,
 } = process.env;
 
-const pusher = new Pusher({
-  appId: channelAppId || '',
-  key: channelKey || '',
-  secret: channelAppSecret || '',
-  cluster: channelCluster || '',
-  useTLS: true,
-  encryptionMasterKeyBase64: encryptionKey,
-});
+// const pusher = new Pusher({
+//   appId: channelAppId || '',
+//   key: channelKey || '',
+//   secret: channelAppSecret || '',
+//   cluster: channelCluster || '',
+//   useTLS: true,
+//   encryptionMasterKeyBase64: encryptionKey,
+// });
 
 type InitFrontChatNextApiRequest = Override<
   NextApiRequest,
