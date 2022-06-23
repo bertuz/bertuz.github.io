7f7030a9d5bbf4985df7f7d7d6a0ab1bcf362064
mbertamini <matteo@bertamini.net>
2022-06-23 21:31:58 +0200

    passing screenshot test on local

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,7 +4,20 @@ module.exports = {
     headless: process.env.HEADLESS,
     ignoreHTTPSErrors: true,
     slowMo: process.env.HEADLESS ? 0 : 50,
-    args: [],
+    args: [
+      '--hide-scrollbars',
+      '--enable-font-antialiasing',
+      '--force-device-scale-factor=1',
+      '--high-dpi-support=1',
+      '--no-sandbox',
+      '--disable-setuid-sandbox', // Props for TravisCI
+      '--font-render-hinting=medium',
+      '--disable-font-subpixel-positioning',
+      '--force-color-profile=generic-rgb',
+      '--enable-font-antialiasing=true',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
   },
   server: {
     command: 'yarn start',
