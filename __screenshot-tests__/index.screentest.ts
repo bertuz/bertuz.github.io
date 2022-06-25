56bf6dbeef9822030ce2be6dad601c52b8c9b9a4
mbertamini <matteo@bertamini.net>
2022-06-25 03:48:45 +0200

    passing screenshot test on local

--- __screenshot-tests__/index.screentest.ts
+++ __screenshot-tests__/index.screentest.ts
@@ -1,10 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 describe('Google', () => {
   beforeAll(async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     await page.goto('http://host.docker.internal:3000');
   });
 
   it('should display "google" text on page', async () => {
     // await expect(page).toMatch('google');
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     const image = await global.page.screenshot();
     expect(image).toMatchImageSnapshot();
   });
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,2 +1,7 @@
 const baseConfig = require('jest-puppeteer-docker/lib/config')();
+
+baseConfig.server = {
+  command: 'yarn start',
+  port: 3000,
+};
 module.exports = baseConfig;
