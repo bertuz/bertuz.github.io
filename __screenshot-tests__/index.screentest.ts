2d090f641f0ca48f932eb48570f281b3a38e574a
mbertamini <matteo@bertamini.net>
2022-07-14 17:51:33 +0200

    updated screenshot test

--- __screenshot-tests__/index.screentest.ts
+++ __screenshot-tests__/index.screentest.ts
@@ -1,12 +1,16 @@
 describe('Google', () => {
   beforeAll(async () => {
-    console.log(`http://${process.env.TEST_URL}:3000`);
-
     await page.goto(`http://${process.env.TEST_URL}:3000`);
   });
 
-  it('should display "google" text on page', async () => {
-    const image = await page.screenshot();
+  it('the home page should be shown according to the design', async () => {
+    let image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+
+    await page.evaluate(() => {
+      window.scrollTo(0, window.document.body.scrollHeight);
+    });
+    image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
   });
 });
