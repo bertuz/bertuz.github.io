66d92f2c799bfd8b328af21bc869cef21521f67b
Matteo Bertamini <matteo@bertamini.net>
2023-03-07 01:34:20 +0100

    add api test for flutter app

--- pages/api/first-flutter/index.ts
+++ pages/api/first-flutter/index.ts
@@ -10,6 +10,32 @@ const places = [
   {
     id: 0,
     name: 'Pratosaiano, Trentino AA, Italy',
+    url: 'http://upload.wikimedia.org/wikipedia/commons/2/23/Pratosaiano_-_Scorcio.jpg',
+    facts: [
+      {
+        title: 'Summary',
+        text: 'bla bla bla bla',
+      },
+      {
+        title: 'Cosas que ver',
+        text: 'una pintura, el blasón',
+      },
+    ],
+  },
+  {
+    id: 0,
+    name: 'Arco, Trentino AA, Italy',
+    url: 'https://mediaim.expedia.com/destination/2/f94473eb4d094460d156a71bb1be9e89.jpg',
+    facts: [
+      {
+        title: 'Summary',
+        text: 'Ciudad del Alto Garda',
+      },
+      {
+        title: 'Cosas que ver',
+        text: 'Una iglesia, heladerías, el centro histórico.',
+      },
+    ],
   },
 ];
 
